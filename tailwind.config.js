bc9cfea9df2eaaeee356bec45b5a2348674d3e4d
keikaloustian <kei.kaloustian@gmail.com>
2023-05-09 19:46:35 -0700

    Add custom Baum colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        moss: "#313b3d",
+        dgray: "#2b2d2a",
+        mgray: "#58595B",
+        lgray: "#d1d3d4",
+        vpurple: "#6E00FF",
+        dpink: "#FF00FF",
+        elime: "#CCFF00",
+      },
+    },
   },
   plugins: [],
 };
