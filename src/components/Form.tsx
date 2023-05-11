bc5f25f2802aabddb7aaa491a0ba1d2c6180fbfc
keikaloustian <kei.kaloustian@gmail.com>
2023-05-11 16:47:56 -0700

    Add phone numbers only mask Add asterisk next to all inputs, all required Add maxLength to all inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,47 +2,68 @@
 
 export default function Form({ formData, setFormData }) {
   return (
-    <form className="col-span-3">
+    <form className="col-span-4 flex flex-col p-12 gap-8">
       {/* NOMBRE */}
-      <label>
-        Nombre:
+      <label className="label">
+        <span className="after:content-['*'] after:text-vpurple after:font-semibold after:inline-block">
+          Tu Nombre:
+        </span>
+        <br />
         <input
           type="text"
           value={formData.nombre || ""}
           onChange={(e) => setFormData({ ...formData, nombre: e.target.value })}
           required
+          className="input"
+          maxLength={50}
         />
       </label>
 
       {/* CELULAR */}
-      <label>
-        Número Celular:
+      <label className="label">
+        <span className="after:content-['*'] after:text-vpurple after:font-semibold after:inline-block">
+          Número Celular:
+        </span>
+        <br />
         <input
-          type="text"
+          type="tel"
           value={formData.celular || ""}
-          onChange={(e) =>
-            setFormData({ ...formData, celular: e.target.value })
-          }
+          onChange={(e) => {
+            // If anything other than a number is typed, do nothing
+            if (e.target.value.match(/\D/)) {
+              return;
+            }
+            setFormData({ ...formData, celular: e.target.value });
+          }}
           required
+          className="input"
+          maxLength={10}
         />
       </label>
 
       {/* EMAIL */}
-      <label>
-        Correo Electrónico:
+      <label className="label">
+        <span className="after:content-['*'] after:text-vpurple after:font-semibold after:inline-block">
+          Correo Electrónico:
+        </span>
+        <br />
         <input
           type="email"
           value={formData.email || ""}
           onChange={(e) => setFormData({ ...formData, email: e.target.value })}
           required
+          className="input"
+          maxLength={50}
         />
       </label>
 
       {/* SCHOLARSHIP OPTIONS */}
-
-      <label className="">
-        Curso 1
+      <p className="text-xl tracking-widest after:content-['*'] after:text-vpurple after:font-semibold after:inline-block">
+        Selecciona un curso para el sorteo de la beca:
+      </p>
+      <div>
         <input
+          id="opt1"
           type="radio"
           value="curso1"
           name="opcion"
@@ -51,10 +72,15 @@ export default function Form({ formData, setFormData }) {
             setFormData({ ...formData, opcion: e.target.value });
           }}
         ></input>
-      </label>
-      <label className="">
-        Curso 2
+        <label className="m-2" htmlFor="opt1">
+          Curso 1
+        </label>
+
+        <br />
+        <br />
+
         <input
+          id="opt2"
           type="radio"
           value="curso2"
           name="opcion"
@@ -63,7 +89,10 @@ export default function Form({ formData, setFormData }) {
             setFormData({ ...formData, opcion: e.target.value });
           }}
         ></input>
-      </label>
+        <label className="m-2" htmlFor="opt2">
+          Curso 2
+        </label>
+      </div>
     </form>
   );
 }
