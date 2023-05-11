6474b9dda914633a8b3d75de2a82324fa97fa1f9
keikaloustian <kei.kaloustian@gmail.com>
2023-05-10 19:37:59 -0700

    Define static metadata title and description for time being

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Baum Music School",
+  description: "Registro",
+};
 
 export default function Home() {
   return <main className="bg-white"></main>;
