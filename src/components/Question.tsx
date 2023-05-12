f6508075dd2caa977ee5b5bb3fb5ae32e3b95038
keikaloustian <kei.kaloustian@gmail.com>
2023-05-12 11:59:40 -0700

    Build preliminary working Question component that updates the question answer correctness state in the parent

---
+++ src/components/Question.tsx
@@ -0,0 +1,34 @@
+export default function Question({ question, step, qResults, setQResults }) {
+  // const obj = {
+  //   prompt: "QUESTION 1",
+  //   alternatives: { a: "this", b: "is", c: "test" },
+  //   answer: "c",
+  // };
+
+  return (
+    <div className="bg-lgray col-span-6 col-start-3 my-[15%]">
+      <h1>Pregunta{step - 1}/5</h1>
+      <h2>{question.prompt}</h2>
+      {/* Iterate over the alternatives object and render for each alt` */}
+      <ol>
+        {Object.entries(question.alternatives).map(([key, value]) => (
+          <li key={key}>
+            <label htmlFor={key}>
+              <input
+                type="radio"
+                id={key}
+                name="alternatives"
+                onChange={() => {
+                  const newResults = [...qResults];
+                  newResults[step - 2] = key === question.answer;
+                  setQResults(newResults);
+                }}
+              ></input>
+              {key + ". " + value}
+            </label>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
