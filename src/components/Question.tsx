6b2baa276d2b9e471bb8739a3649ea715e6935ea
keikaloustian <kei.kaloustian@gmail.com>
2023-05-12 18:50:10 -0700

    Style Question screen

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -5,31 +5,47 @@ export default function Question({ question, step, qResults, setQResults }) {
   //   answer: "c",
   // };
 
+  const randomizer = Math.random();
+  const randColour =
+    randomizer <= 0.33
+      ? "bg-elime"
+      : randomizer >= 0.66
+      ? "bg-dpink"
+      : "bg-vpurple";
+
   return (
-    <div className="bg-lgray col-span-6 col-start-3 my-[15%]">
-      <h1>Pregunta{step - 1}/5</h1>
-      <h2>{question.prompt}</h2>
-      {/* Iterate over the alternatives object and render for each alt` */}
-      <ol>
-        {Object.entries(question.alternatives).map(([key, value]) => (
-          <li key={key}>
-            <label htmlFor={key}>
-              <input
-                type="radio"
-                id={key}
-                name="alternatives"
-                onChange={() => {
-                  // Update qResults state with bool answer is correct or not
-                  const newResults = [...qResults];
-                  newResults[step - 2] = key === question.answer;
-                  setQResults(newResults);
-                }}
-              ></input>
-              {key + ". " + value}
-            </label>
-          </li>
-        ))}
-      </ol>
-    </div>
+    <>
+      <div className={`${randColour} h-screen w-[38%] absolute right-0`}></div>
+      <div className=" col-span-7 col-start-2 relative">
+        <h1 className="text-5xl tracking-wider font-light flex justify-between my-[15%]">
+          Pregunta
+          <div className="bg-dgray ml-4 mr-8 h-[1px] w-full self-end"></div>
+          <span className="tracking-[1em]">{step - 1}/5</span>
+        </h1>
+        <h2 className="text-2xl my-16">{question.prompt}</h2>
+        {/* Iterate over the alternatives object and render for each alt` */}
+        <ol className="flex flex-col gap-6">
+          {Object.entries(question.alternatives).map(([key, value]) => (
+            <li key={key}>
+              <label htmlFor={key} className="text-xl inline-block">
+                <input
+                  type="radio"
+                  id={key}
+                  name="alternatives"
+                  onChange={() => {
+                    // Update qResults state with bool answer is correct or not
+                    const newResults = [...qResults];
+                    newResults[step - 2] = key === question.answer;
+                    setQResults(newResults);
+                  }}
+                  className="m-3 accent-vpurple"
+                ></input>
+                {key + ". " + value}
+              </label>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
   );
 }
