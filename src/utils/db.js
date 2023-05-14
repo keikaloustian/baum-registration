50da9fd1c44a021a81e22619555b6c4891205eff
keikaloustian <kei.kaloustian@gmail.com>
2023-05-13 19:26:39 -0700

    Move db connection into api route handler

--- src/utils/db.js
+++
@@ -1,5 +0,0 @@
-const mysql = require("mysql2");
-
-const connection = mysql.createConnection(
-  `mysql://${process.env.PLANETSCALE_DB_USERNAME}:${process.env.PLANETSCALE_DB_PASSWORD}@${process.env.PLANETSCALE_DB_HOST}/${PLANETSCALE_DB}?ssl={"rejectUnauthorized":true}`
-);
